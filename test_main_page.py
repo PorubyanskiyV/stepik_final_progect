cbf409a253cec3fa06bdf52eb7b02f457418d59f
porubianskyi <porubianskyi@zfort.com>
2019-10-11 16:24:05 +0300

    Create Base Page class

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
