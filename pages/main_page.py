84674db4c15f338460462a40f909d2c7ec3457a1
porubianskyi <porubianskyi@zfort.com>
2019-10-11 19:06:27 +0300

    Add handling exception

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,11 @@
 from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element_by_css_selector("#login_link")
         link.click()
 
-        ####
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+####
